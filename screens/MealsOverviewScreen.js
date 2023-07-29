8efdb7211f9dc4cf14c5cc6d71a3942240329dd4
Rainers Lovkins <62895879+RainersCode@users.noreply.github.com>
2023-07-29 15:37:31 +0300

    check

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,56 @@
+import { useLayoutEffect } from "react";
+import { MEALS , CATEGORIES } from "../data/dummy-data";
+import { StyleSheet, View, FlatList } from "react-native";
+import MealItem from "../components/MealItem";
+
+function MealsOverviewScreen({ route, navigation }) {
+const catId = route.params.categoryId;
+
+const displayedMeals = MEALS.filter((mealItem) => {
+  return mealItem.categoryIds.indexOf(catId) >= 0;
+})
+
+useLayoutEffect(()=>{
+  const categoryTitle = CATEGORIES.find(
+    (category) => category.id === catId
+  ).title;
+
+  navigation.setOptions({
+    title: categoryTitle
+  });
+}, [catId, navigation])
+
+
+function renderMealItem(itemData){
+
+ 
+  
+  const item = itemData.item;
+  const mealItemProps = {
+    title: item.title,
+    imageUrl: item.imageUrl,
+    affordability: item.affordability,
+    complexity: item.complexity,
+    duration: item.duration,
+    id: item.id
+  };
+  return (
+    <MealItem {...mealItemProps} />
+  );
+}
+
+  return (
+    <View style={styles.container}>
+      <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem}/>
+    </View>
+  );
+};
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16
+    }
+});
